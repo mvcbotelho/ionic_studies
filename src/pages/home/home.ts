b1187e8ba27bfbb91958b31b14a08709938f3e16
Marcus Botelho <marcus.botelho@ifactory.com.br>
2018-12-09 20:35:40 -0200

    initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,30 @@
+import {Component, Injectable} from '@angular/core';
+// import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import { NavController } from 'ionic-angular';
+import "rxjs/add/operator/map";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+@Injectable()
+export class HomePage {
+
+  private url: string = 'https://api.github.com/users'
+
+  constructor(
+    public navCtrl: NavController,
+    public http: HttpClient
+    ) {
+  }
+
+  callUser() {
+    this.http.get(this.url + '/mvcbotelho')
+      .map(res => {
+        console.log('Response', res)
+      })
+      .subscribe(data => console.log('Data - ', data))
+  }
+}
